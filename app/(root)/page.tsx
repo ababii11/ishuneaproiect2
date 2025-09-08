1d8dd7e89725fc04144ed15d35cf0ce0e16cdef7
ionel.ababii@isa.utm.md <ionel.ababii@isa.utm.md>
2025-09-08 22:19:49 +0300

    Primul commit, startul proiectului

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="p-4 space-y-4">
+      <div>
+        <Button>Click me</Button>
+      </div>
+      <p>Hello Admin Dashboard</p>
+    </main>
+  );
+}
+
+
