66cdef3261f5ce13fd99a79557117266aa0fd681
ionel.ababii@isa.utm.md <ionel.ababii@isa.utm.md>
2025-09-09 12:34:38 +0300

    Autenificarea clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,16 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
-export default function Home() {
+const Setuppage = () => {
   return (
     <main className="p-4 space-y-4">
       <div>
-        <Button>Click me</Button>
+       This is a proected route!
       </div>
       <p>Hello Admin Dashboard</p>
     </main>
   );
 }
 
+export default Setuppage;
 
