ac2b616b7c940ff9102d3487a53976ca6d3dbde6
ionel.ababii@isa.utm.md <ionel.ababii@isa.utm.md>
2025-09-16 18:34:47 +0300

    Probleme baza date

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider, SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import { ModalProvider } from "@/providers/modal-provider";
 const geistSans = Inter({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -26,6 +27,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <ModalProvider/>
           <header className="p-4 border-b flex items-center justify-between">
             <div className="font-semibold">Ecommerce Admin</div>
             <div className="flex items-center gap-2">
